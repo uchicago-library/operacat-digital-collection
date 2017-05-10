2b96f5a661f06753079b86bba68f6d36089038d6
Tyler Danstrom <tdanstrom@uchicago.edu>
2017-05-10 16:16:39 -0500

    issue #12; bugfixes
    fixed paginating advanced search but now introduced a new bug. need to
    nail down precisely how to filter on Orderables. also: javascript
    functionality to filter out inputs to forms with '---' as value?

--- operacat/search/views.py
+++ operacat/search/views.py
@@ -9,7 +9,7 @@ from wagtail.wagtailcore.models import Page
 from wagtail.wagtailsearch.models import Query
 from wagtail.wagtailsearch.backends import get_search_backend
 
-from catalogitems.models import CatalogItemPage, Composer, Dealer, Catalog
+from catalogitems.models import *
 
 def search(request):
     search_query = request.GET.get('query', None)
@@ -32,48 +32,57 @@ def search(request):
 
                  )
 
-"""
+
 def advanced_search(request):
-    composer_query = request.GET.get('composer-query', None) if request.GET.get('composer-query', None) != 'none' else None
-    dealer_query = request.GET.get('dealer-query', None) if request.GET.get('dealer-query', None) != 'none' else None
-    catalog_query = request.GET.get('catalog-query', None) if request.GET.get('catalog-query', None) != 'none' else None
-    title_query = request.GET.get('title-query', None) if request.GET.get('title-query', None) != 'none' else None
-    place_query = request.GET.get('place-query', None) if request.GET.get('place-query', None) != 'none' else None
-    start_year_query = request.GET.get('start-year-query', None) if request.GET.get('start-year-query', None) != 'none' else None
-    end_year_query = request.GET.get('end-year-query', None) if request.GET.get('end-year-query', None) != 'none' else None
-    page = request.GET.get('page', 1)
-    search_results = CatalogItemPage.objects
-    if composer_query and composer_query != "---":
-        composer = Composer.objects.filter(last_name=composer_query)[0]
-        search_results = search_results.filter(item_composer=composer)
-    elif dealer_query and dealer_query != "---":
-        dealer = Dealer.objects.filter(dealer_name=dealer_query)[0]
-        search_results.filter(item_dealer=dealer)
-    elif catalog_query and catalog_query != "---":
-        catalog = Catalog.objects.filter(dealer_name=catalog_query)[0]
-        search_results.filter(item_catalog=catalog)
-    elif title_query and title_query != "---":
-        titles = PieceTitle.objects.filter(title_name=title_query)[0]
-        search_results.filter(item_titles__in=titles)
-    elif place_query and place_query != "---":
-        places = Place.objects.filter(place_name=title_query)[0]
-        search_results.filter(item_places=places)
-    elif start_year_query and start_year_query != "---":
-        pass
-    elif end_year_query and end_year_query != "---":
-        pass
-    if search_results.count() > 0:
-        search_results = search_results
-    else:
-        search_results = CatalogItemPage.objects.none()
-    paginator = Paginator(search_results, 100)
+    composer_query = request.GET.get('composer-query', None)
+    dealer_query = request.GET.get('dealer-query', None)
+    catalog_query = request.GET.get('catalog-query', None)
+    place_query = request.GET.get('place-query', None)
+    title_query = request.GET.get('title-query', None)
+    item_type_query = request.GET.get('item-type-query', None)
+
     page_num = int(request.GET.get("page", 1))
+
+    search_results = CatalogItemPage.objects.all()
+    if composer_query:
+        composer = Composer.objects.filter(last_name=composer_query)
+        search_results = search_results.filter(item_composer=composer[0])
+    if dealer_query:
+        dealer = Dealer.objects.filter(dealer_name=dealer_query)
+        search_results = search_results.filter(item_dealer=dealer[0])
+    if catalog_query:
+        catalog = Catalog.objects.filter(catalog_name=catalog_query)
+        search_results = search_results.filter(item_catalog=catalog[0])
+    if place_query:
+        places = Place.objects.filter(place_name=place_query)[0]
+        places = PlaceOrderable.objects.filter(a_place=places)[0]
+        search_results = search_results.filter(item_places=composer)
+    if title_query:
+        titles = PieceTitle.objects.filter(name__contains=title_query)[0]
+        titles = PieceTitleOrderable.objects.filter(a_title=titles)[0]
+        search_results = search_results.filter(item_titles=titles)
+    if item_type_query:
+        item_types = ItemType.objects.filter(type_name=item_type_query)[0]
+        item_types = ItemTypeOrderable.objects.filter(a_type=item_types)[0]
+        search_results = search_results.filter(item_types=item_types)
+    search_query = "{},{},{},{},{},{}".format(composer_query, dealer_query, catalog_query, place_query, title_query, item_type_query)
+    query = Query.get(search_query)
+    query.add_hit()
+    paginator = Paginator(search_results, 100)
     try:
         search_results = paginator.page(page_num)
     except EmptyPage:
         search_results = []
+
     return render(request,
-                  "search/search.html",
-                  {'search_results':search_results}
+                  'search/search.html',
+                  {'search_results': search_results,
+                   'composer_query': composer_query,
+                   'dealer_query': dealer_query,
+                   'catalog_query': catalog_query,
+                   'place_query': place_query,
+                   'title_query': title_query,
+                   'item_type_query': item_type_query}
                  )
-"""
+
+
