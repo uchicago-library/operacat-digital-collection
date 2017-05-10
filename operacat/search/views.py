623998d5dead5d73006e0b1bc016f14f1379df55
Tyler Danstrom <tdanstrom@uchicago.edu>
2017-05-10 13:42:38 -0500

    issue #12; bugs in syntax of advanced_search return value

--- operacat/search/views.py
+++ operacat/search/views.py
@@ -32,11 +32,15 @@ def search(request):
 
                  )
 
-
+"""
 def advanced_search(request):
     composer_query = request.GET.get('composer-query', None) if request.GET.get('composer-query', None) != 'none' else None
     dealer_query = request.GET.get('dealer-query', None) if request.GET.get('dealer-query', None) != 'none' else None
     catalog_query = request.GET.get('catalog-query', None) if request.GET.get('catalog-query', None) != 'none' else None
+    title_query = request.GET.get('title-query', None) if request.GET.get('title-query', None) != 'none' else None
+    place_query = request.GET.get('place-query', None) if request.GET.get('place-query', None) != 'none' else None
+    start_year_query = request.GET.get('start-year-query', None) if request.GET.get('start-year-query', None) != 'none' else None
+    end_year_query = request.GET.get('end-year-query', None) if request.GET.get('end-year-query', None) != 'none' else None
     page = request.GET.get('page', 1)
     search_results = CatalogItemPage.objects
     if composer_query and composer_query != "---":
@@ -48,6 +52,16 @@ def advanced_search(request):
     elif catalog_query and catalog_query != "---":
         catalog = Catalog.objects.filter(dealer_name=catalog_query)[0]
         search_results.filter(item_catalog=catalog)
+    elif title_query and title_query != "---":
+        titles = PieceTitle.objects.filter(title_name=title_query)[0]
+        search_results.filter(item_titles__in=titles)
+    elif place_query and place_query != "---":
+        places = Place.objects.filter(place_name=title_query)[0]
+        search_results.filter(item_places=places)
+    elif start_year_query and start_year_query != "---":
+        pass
+    elif end_year_query and end_year_query != "---":
+        pass
     if search_results.count() > 0:
         search_results = search_results
     else:
@@ -60,7 +74,6 @@ def advanced_search(request):
         search_results = []
     return render(request,
                   "search/search.html",
-                  {
-                   'search_results': search_results}
+                  {'search_results':search_results}
                  )
-
+"""
