67aa4b0ea36be9028594187cbfb18346b3eea237
Tyler Danstrom <tdanstrom@uchicago.edu>
2017-05-20 13:02:16 -0500

    added descriptions, author info to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup
 
+def readme():
+    with open("README.md", "r") as read_file:
+        return read_file.read()
+
 setup(
     name='operaCat',
     version='1.0',
-    long_description=__doc__,
+    author = ['Tyler Danstrom', 'Kathy Zadrozny'],
+    author_email = ['tdanstrom@uchicago.edu', 'kzadrozny@uchicago.edu'],
+    description="A website developed by the authors for the Digital Library Development to present records of items collected from auction sales about five famous Italian composers",
+    license = "LGPL3.0",
+    long_description=readme(),
     packages=['operacat'],
     include_package_data=True,
     zip_safe=False,
