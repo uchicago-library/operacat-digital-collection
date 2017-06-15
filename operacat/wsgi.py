00ea8fe5b436272f056dcdfa515ff9e54a50b592
Tyler Danstrom <tdanstrom@uchicago.edu>
2017-06-15 14:31:09 -0500

    added forced path appends to wsgi file

--- operacat/wsgi.py
+++ operacat/wsgi.py
@@ -8,11 +8,13 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 from __future__ import absolute_import, unicode_literals
-
 import os
+import sys
+
+sys.path.append("/data/recitative/venvs/operacatENV/lib/python3.5/site-packages")
+sys.path.append("/data/recitative/sites/operacat-digital-collection/operacat")
 
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "operacat.settings.dev")
 
 application = get_wsgi_application()
